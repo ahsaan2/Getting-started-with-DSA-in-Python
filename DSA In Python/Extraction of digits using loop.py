bbad48b7fb888f50b97d50ac97a0be36d83b2ba5
Ahsaan <ahsaan1345@outlook.com>
2025-08-11 23:46:03 +0530

    Basic questions

--- DSA In Python/Extraction of digits using loop.py
+++ DSA In Python/Extraction of digits using loop.py
@@ -1,6 +1,8 @@
 # (1). Count number of digits
 import numbers
 
+from Dictionaries import empty_dict
+
 n = 5873
 while n > 0:
     rem = n % 10
@@ -20,6 +22,7 @@ print("number of digits: ", count)
 
 # (3) check if a number is palindrome or not
 n = 121
+# print("number of digits: ", len(str(n)))
 reserved = n
 reversed_num = 0
 while n > 0:
@@ -59,3 +62,19 @@ if total == reserve_value:
     print("Is a armstrong number")
 else:
     print("Not a armstrong number")
+
+# (5) Given a number , return the number of divisors in a list
+given_number = 10
+empty_list = []
+
+# for i in range(1, given_number + 1):
+for i in range(1, given_number + 1 // 2):
+    # if i > given_number / 2 and i != given_number:
+    #     continue
+    # if i > given_number / 2:
+    #     break
+    if given_number % i == 0:
+        empty_list.append(i)
+empty_list.append(given_number)
+
+print(empty_list)
